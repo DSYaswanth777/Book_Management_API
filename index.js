2aeab41104af321f7d0ff80abad86a12fec97bdb
DSYaswanth777 <dsyaswanth777@gmail.com>
2021-08-18 22:40:39 +0530

    Added required APIs for Project

---
+++ index.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const booky = express();
+booky.listen(3000,()=> console.log("Hey server is running fine"))
---
+++ requirments.js
@@ -0,0 +1,72 @@
+// Requirements for our Project
+
+// We are a book management company
+
+// Books
+// ISBN, title, pub date, langauge, num page, author[], category[]
+
+// Authors
+// id, name, books[]
+
+// Publications
+// id, name, books[]
+
+// What are all the APIs that we need.....
+
+// Books
+// We need an API
+
+// GET
+// to get all books 
+// to get specific books
+// to get list of books based on category
+// to get list of books based on languages [task]
+
+// POST
+// Add new book
+
+// PUT
+// Update book title
+// update/add new author for a book
+
+// DELETE
+// delete a book
+// delete an author
+
+// Authors
+
+// We need an api
+
+// GET
+// to get all authors
+// to get specific authors [task]
+// to get list of authors based on books 
+
+// POST
+// Add new author
+
+// PUT
+// Update Author name using it's id [Task] 
+
+// DELETE
+// Delete an author
+
+// Publications
+
+// we need an API
+
+// GET
+// to get all publication  
+// to get specific publication [task] 
+// to get list of publication based on book [task] 
+
+// POST
+// Add new publications [Task] 
+
+// PUT
+// UPdate the publication name using it's id [Task]
+// update/add books to publications
+
+// DELETE
+// Delete the publication
+// delete a book from publication
