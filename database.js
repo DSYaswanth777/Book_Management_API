159ec47cb88991171ce124311d0d3ea9e4ef3630
DSYaswanth777 <dsyaswanth777@gmail.com>
2021-08-19 10:01:10 +0530

    Added data base

---
+++ database.js
@@ -0,0 +1,32 @@
+const books =[
+    {
+        ISBN: "12345Book",
+        title: "Getting started with MERN",
+        pubDate:"2021-08-08",
+        language: "ene",
+        Pagenum:250,
+        Author:[1,2],
+        publication: [1],
+        category:["tech","programming","education","thriller"]
+    },
+];
+const author = [
+    {
+        id:1,
+        name:"Yaswanth",
+        books:["12345Book"],
+    },
+    {
+        id:2,
+        name:"Elon Musk",
+        books:["12345Book"],
+    }
+];
+
+const publication =[
+    {
+        id:1,
+        name: "Vikrant",
+        books:["12345Book"],
+    }
+];
